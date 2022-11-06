f64fb674165b99f82a268461be7367235ea85a29
Evans Ochieng <evansochieng121@gmail.com>
2022-11-06 16:39:50 +0300

    create web API using sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationController < Sinatra::Base
+  #Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+
+    # get all the games from the database and order by title
+    games = Game.all.order(:title).limit(10)
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    # include associated reviews in the JSON response
+    #game.to_json(include: :reviews)
+
+    #Also include users associated with every review
+    #game.to_json(include: { reviews: { include: :user } })
+
+    #Be selective on the attributes you want returned
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
